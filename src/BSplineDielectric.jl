a64e56e9f7d42fc81d842cb47d56c814f1c27379
stakahama <satoshi.takahama@gmail.com>
2025-07-21 14:14:28 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/BSplineDielectric.jl
@@ -0,0 +1,5 @@
+module BSplineDielectric
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BSplineDielectric
+using Test
+
+@testset "BSplineDielectric.jl" begin
+    # Write your tests here.
+end
